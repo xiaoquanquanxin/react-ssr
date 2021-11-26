371c67873a17620eb506e9915fb5cadf4bd77944
quanxin <quanxin@pujitech.com>
2021-11-26 17:44:24 +0800

    是否重新发请求

--- src/shared/components/about.tsx
+++ src/shared/components/about.tsx
@@ -8,6 +8,10 @@ function About() {
 		const [data, setData] = useReducer(reducer, getInitialState());
 		useEffect(() => {
 				(async () => {
+						//  是否重新发请求
+						if (data && data.name) {
+								return;
+						}
 						const result = await requestGetData() as initialState;
 						result.name += ' 此为请求返回';
 						setData({type: 'set', newData: result});
