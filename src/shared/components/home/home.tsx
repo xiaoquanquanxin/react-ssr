34f00ea9e0011ec0eb1de3b25eae902bcaa2a518
quanxin <quanxin@pujitech.com>
2021-12-03 16:23:34 +0800

    addProduction.tsx

--- src/shared/components/home/home.tsx
+++ src/shared/components/home/home.tsx
@@ -5,10 +5,10 @@ import {requestGetData} from "@shared/request";
 function Home() {
 		const [data, getData] = useState(null);
 		useEffect(() => {
-				(async () => {
-						const result = await requestGetData({home: 'home'}) as initialState;
-						getData(result);
-				})();
+				// (async () => {
+				// 		const result = await requestGetData({home: 'home'}) as initialState;
+				// 		getData(result);
+				// })();
 		}, []);
 		return (
 				<div>
---
+++ src/shared/components/production/addProduction/addProduction.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function AddProduction(props) {
+		console.log('AddProduction 的 props', props);
+		return (
+				<div>
+						<h2>AddProduction</h2>
+				</div>
+		);
+}
+
+export default AddProduction;
