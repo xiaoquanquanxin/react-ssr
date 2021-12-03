e9af9d53ca1adffe757881510fbfe723f637e0c1
quanxin <quanxin@pujitech.com>
2021-12-03 17:27:27 +0800

    首页

--- src/shared/components/home/home.tsx
+++ src/shared/components/home/home.tsx
@@ -1,19 +1,9 @@
 import * as React from 'react';
-import {useEffect, useState} from "react";
-import {requestGetData} from "@shared/request";
 
 function Home() {
-		const [data, getData] = useState(null);
-		useEffect(() => {
-				// (async () => {
-				// 		const result = await requestGetData({home: 'home'}) as initialState;
-				// 		getData(result);
-				// })();
-		}, []);
 		return (
 				<div>
-						<h2>Home</h2>
-						<p>{data && data.name}</p>
+						<h2>首页</h2>
 				</div>
 		);
 }
