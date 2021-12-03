315fa92e0f1fb89a4929dfaa9d1e9aeb74912daf
xiaoquanquanxin <xxkl123@126.com>
2021-12-03 21:11:05 +0800

    dev tool

--- build/client/webpack.client.js
+++ build/client/webpack.client.js
@@ -1,21 +1,22 @@
-const {getDirFn, webpackResolve, rules: _rules} = require('../constants');
+const { getDirFn, webpackResolve, rules: _rules } = require('../constants');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const {merge} = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge');
 const rules = [..._rules];
 rules.splice(1, 0,
     {
         test: /\.style.module.(css|less)$/,
         use: [
             {
-                loader: 'style-loader'
+                loader: 'style-loader',
             },
         ],
-    }
-)
+    },
+);
 console.log(rules);
 module.exports = merge({
+    devtool: 'source-map',
     output: {
         filename: 'js/bundle.[contenthash:4].js',
         path: getDirFn('dist/client'),
@@ -34,13 +35,13 @@ module.exports = merge({
     },
     plugins: [
         new CleanWebpackPlugin({
-            cleanOnceBeforeBuildPatterns: getDirFn('dist')
+            cleanOnceBeforeBuildPatterns: getDirFn('dist'),
         }),
         new HtmlWebpackPlugin({
             template: getDirFn('src/client/template.html'),
             filename: getDirFn('dist/client/index.html'),
             //  注意不能删除注释
-            minify: {removeComments: false,}
+            minify: { removeComments: false },
         }),
         new MiniCssExtractPlugin(),
     ],
