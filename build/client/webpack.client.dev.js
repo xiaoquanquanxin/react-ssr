f207dd23f1e04cccbdcb3c1f4dfff135e2d6aad0
quanxin <quanxin@pujitech.com>
2021-12-03 18:26:14 +0800

    source map

--- build/client/webpack.client.dev.js
+++ build/client/webpack.client.dev.js
@@ -1,10 +1,30 @@
 const {merge} = require('webpack-merge');
 const server = require('./webpack.client');
 const {getDirFn} = require('../constants');
+const webpack = require('webpack');
 const mode = 'development';
 module.exports = merge(server, {
     entry: {
         index: getDirFn('src/client/index.tsx'),
     },
     mode,
+    module: {
+        rules: [
+            {
+                test: /\.ts(x)$/,
+                enforce: "pre",
+                use: [
+                    {
+                        loader: "source-map-loader",
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.SourceMapDevToolPlugin({
+            filename: '[file].map',
+            module: true,
+        })
+    ],
 });
